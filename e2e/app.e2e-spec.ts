0b7b058745d650f75c232e309acc2fa7483c21b0
Leonardod <lfdantoni@gmail.com>
2017-05-01 18:20:28 -0300

    init

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { Angular2ContactsPage } from './app.po';
+
+describe('angular2-contacts App', function() {
+  let page: Angular2ContactsPage;
+
+  beforeEach(() => {
+    page = new Angular2ContactsPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
